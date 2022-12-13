7facba6250421e1be93812cf8e008ed16be7a53c
hung279 <hungnher279@gmail.com>
2022-12-13 14:53:00 +0700

    init Buoi9 and btvn

---
+++ Buoi9/BTVN/bai1.js
@@ -0,0 +1,5 @@
+// if else for
+//viết hàm isPrime kiểm tra số nguyên tố
+
+console.log(isPrime(7)) //output: true;
+console.log(isPrime(10)) //output: false;
---
+++ Buoi9/BTVN/bai2.js
@@ -0,0 +1,19 @@
+//cho object 
+
+const obj = {
+    name: "Hưng",
+    age: 20,
+    email: "dth27902@gamil.com"
+}
+
+//viết hàm run xử lý sao cho ra kết quả là một mảng như output dưới đây
+//gợi ý: khởi tạo 1 array
+
+console.log(run(obj));
+// Output:
+// [
+//      "name: Hưng",
+//      "age: 20",
+//      "emai: dth27902@gmail.com"
+// ]
+
