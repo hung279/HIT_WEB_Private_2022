8cbd2bcde5b30628cf5304618c0a8d1cdb13c8bb
hung279 <hungnher279@gmail.com>
2022-12-22 00:46:43 +0700

    "solution bai 2"

--- Buoi9/BTVN/bai2.js
+++ Buoi9/BTVN/bai2.js
@@ -1,13 +1,21 @@
-//cho object 
+//cho object
 
 const obj = {
-    name: "Hưng",
-    age: 20,
-    email: "dth27902@gamil.com"
+  name: "Hưng",
+  age: 20,
+  email: "dth27902@gamil.com"
 }
 
 //viết hàm run xử lý sao cho ra kết quả là một mảng như output dưới đây
 //gợi ý: khởi tạo 1 array
+
+const run = (object) => {
+  let arr = [];
+  for (const key in object) {
+    arr.push(key + ": " + object[key]);
+  }
+  return arr;
+};
 
 console.log(run(obj));
 // Output:
@@ -16,4 +24,3 @@ console.log(run(obj));
 //      "age: 20",
 //      "emai: dth27902@gmail.com"
 // ]
-
