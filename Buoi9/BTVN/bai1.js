4e8b15d45e5931c469508c68cee0421aef9bfed9
hung279 <hungnher279@gmail.com>
2023-01-05 11:47:55 +0700

    add Buoi11

--- Buoi9/BTVN/bai1.js
+++ Buoi9/BTVN/bai1.js
@@ -1,5 +1,14 @@
 // if else for
 //viết hàm isPrime kiểm tra số nguyên tố
 
-console.log(isPrime(7)) //output: true;
-console.log(isPrime(10)) //output: false;
+// console.log(isPrime(7)) //output: true;
+// console.log(isPrime(10)) //output: false;
+
+const ss = (str) => {
+    console.log(str.split(/#+/));
+    for (const s of str) {
+        
+    }
+}
+
+ss("####abdfdfd # ghghghg## fgfgf  #");
